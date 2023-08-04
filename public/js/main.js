a6c0d23b348b45a685caee0d3ddfa8efbc0b6be6
Charlie <cbronson@gmail.com>
2023-08-04 11:04:54 -0500

    refactored main

--- public/js/main.js
+++ public/js/main.js
@@ -3,69 +3,60 @@ import Grid from "./core/grid.js";
 import Globe from "./core/globe.js";
 
 class App {
-  static grid;
-  static gridElement;
+  constructor() {
+    this.grid = new Grid(120, 60);
+    this.gridElement = document.querySelector(".grid");
+    this.version = new Version().getVersion();
+    this.highlightedRegions = [
+      { name: "UnitedStates", className: "cell-usa" },
+      { name: "Canada", className: "cell-canada" },
+      { name: "Greenland", className: "cell-greenland" },
+      { name: "Iceland", className: "cell-iceland" },
+      { name: "Ireland", className: "cell-ireland" },
+      { name: "UnitedKingdom", className: "cell-united-kingdom" },
+      { name: "Norway", className: "cell-norway" },
+      { name: "Sweden", className: "cell-sweden" },
+      { name: "Finland", className: "cell-finland" },
+      { name: "Estonia", className: "cell-estonia" },
+      { name: "Latvia", className: "cell-latvia" },
+      { name: "Lithuania", className: "cell-lithuania" },
+      { name: "France", className: "cell-france" },
+      { name: "Portugal", className: "cell-portugal" },
+      { name: "Spain", className: "cell-spain" },
+      { name: "Italy", className: "cell-italy" },
+      { name: "Belgium", className: "cell-belgium" },
+      { name: "Mexico", className: "cell-mexico" },
+      { name: "Cuba", className: "cell-cuba" },
+      // Add more regions as needed
+    ];
+  }
 
-  static init() {
-    this.initializeGrid();
+  init() {
     this.createGlobe();
     this.displayGrid();
     this.updateVersionText();
     this.highlightRegions();
   }
 
-  static initializeGrid() {
-    this.grid = new Grid(120, 60);
-    this.gridElement = document.querySelector(".grid");
-  }
-
-  static highlightRegions() {
-    this.highlightRegion("UnitedStates", "cell-usa");
-    this.highlightRegion("Canada", "cell-canada");
-    this.highlightRegion("Greenland", "cell-greenland");
-    this.highlightRegion("Iceland", "cell-iceland");
-    this.highlightRegion("Ireland", "cell-ireland");
-    this.highlightRegion("UnitedKingdom", "cell-united-kingdom");
-    this.highlightRegion("Norway", "cell-norway");
-    this.highlightRegion("Sweden", "cell-sweden");
-    this.highlightRegion("Finland", "cell-finland");
-    this.highlightRegion("Estonia", "cell-estonia");
-    this.highlightRegion("Latvia", "cell-latvia"); 
-    this.highlightRegion("Lithuania", "cell-lithuania");
-    this.highlightRegion("France", "cell-france");
-    this.highlightRegion("Portugal", "cell-portugal");
-    this.highlightRegion("Spain", "cell-spain");
-    this.highlightRegion("Italy", "cell-italy");
-    this.highlightRegion("Belgium", "cell-belgium");
-    this.highlightRegion("Mexico", "cell-mexico");
-    this.highlightRegion("Cuba", "cell-cuba");
-  }
-  
-  static highlightRegion(region, className) {
-    const states = Globe[`get${region}`]();
-    states.forEach(cell => {
-      const [x, y] = cell; // Extract the coordinates from the cell
-      this.grid.addClassToCell(x, y, className);
+  highlightRegions() {
+    this.highlightedRegions.forEach((region) => {
+      const states = Globe[`get${region.name}`]();
+      states.forEach((cell) => {
+        const [x, y] = cell;
+        this.grid.addClassToCell(x, y, region.className);
+      });
     });
-  
-  }
-  
-
-  static updateVersionText() {
-    const ver = new Version();
-    const version = ver.getVersion();
-    this.setPageTitle(version);
-  }
-  
-  static setPageTitle(version) {
-    document.title = `World Map: Version ${version}`;
   }
 
-  static createGlobe() {
+  updateVersionText() {
+    document.title = `World Map: Version ${this.version}`;
+  }
+
+  createGlobe() {
     Globe.createGlobe(this.grid);
   }
 
-  static displayGrid(targetX = -1, targetY = -1, direction = "") {
+  displayGrid(targetX = -1, targetY = -1, direction = "") {
     this.gridElement.innerHTML = "";
 
     for (let y = 0; y < this.grid.height; y++) {
@@ -81,6 +72,8 @@ class App {
 
         if (this.grid.isLand(x, y)) {
           cellElement.classList.add("land");
+          const countryName = this.getCountryNameFromCoordinates(x, y);
+          cellElement.dataset.countryName = countryName;
         }
 
         if (this.grid.isWater(x, y)) {
@@ -101,16 +94,52 @@ class App {
           cellElement.classList.add("selected");
         });
 
+        cellElement.addEventListener("mouseover", () => {
+          const countryName = cellElement.dataset.countryName;
+          if (countryName) {
+            console.log("Hovered over:", countryName);
+            // You can display the country name in a tooltip or any other way you prefer
+          }
+        });
+
         this.gridElement.appendChild(cellElement);
       }
     }
 
     this.grid.addClassToCell(2, 3, "highlight");
     this.grid.setCellDataAttribute(2, 3, "test", "ABC");
+  }
 
+  getCountryNameFromCoordinates(x, y) {
+    // Country mappings based on grid coordinates
+    const countryMap = {
+      2: { 3: "UnitedStates" },
+      10: { 20: "Canada" },
+      30: { 10: "Greenland" },
+      40: { 50: "Iceland" },
+      20: { 40: "Ireland" },
+      15: { 45: "UnitedKingdom" },
+      70: { 20: "Norway" },
+      75: { 20: "Sweden" },
+      80: { 20: "Finland" },
+      85: { 20: "Estonia" },
+      80: { 30: "Latvia" },
+      75: { 30: "Lithuania" },
+      25: { 30: "France" },
+      20: { 25: "Portugal" },
+      25: { 20: "Spain" },
+      35: { 25: "Italy" },
+      30: { 25: "Belgium" },
+      5: { 10: "Mexico" },
+      10: { 10: "Cuba" },
+      // Add more country mappings as needed
+    };
+
+    return countryMap[x]?.[y] || "";
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  App.init();
+  const app = new App();
+  app.init();
 });

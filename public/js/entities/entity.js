15c1a244b694de291eb8063a1a52ccec16499ab2
Charlie Schmidt <cbronson@gmail.com>
2023-09-30 10:50:12 -0500

    formatting

--- public/js/entities/entity.js
+++ public/js/entities/entity.js
@@ -1,25 +1,25 @@
 class Entity {
-    constructor(x, y, hitPoints) {
-      this.x = x;
-      this.y = y;
-      this.hitPoints = hitPoints;
-      this.inventory = [];
-    }
+  constructor(x, y, hitPoints) {
+    this.x = x;
+    this.y = y;
+    this.hitPoints = hitPoints;
+    this.inventory = [];
+  }
 
-    addItemToInventory(item) {
-      this.inventory.push(item);
-    }
+  addItemToInventory(item) {
+    this.inventory.push(item);
+  }
 
-    removeItemFromInventory(item) {
-      const index = this.inventory.indexOf(item);
-      if (index !== -1) {
-        this.inventory.splice(index, 1);
-      }
-    }
-
-    hasItemInInventory(item) {
-      return this.inventory.includes(item);
+  removeItemFromInventory(item) {
+    const index = this.inventory.indexOf(item);
+    if (index !== -1) {
+      this.inventory.splice(index, 1);
     }
   }
 
-  export default Entity;
+  hasItemInInventory(item) {
+    return this.inventory.includes(item);
+  }
+}
+
+export default Entity;

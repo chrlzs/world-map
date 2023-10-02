a0725a809cf48bf059800078d852b344c4d2bdff
Charlie Schmidt <cbronson@gmail.com>
2023-10-02 17:21:11 -0500

    formatting

--- public/js/core/set.js
+++ public/js/core/set.js
@@ -1,32 +1,31 @@
 class Set {
-    constructor() {
-      this.items = {};
-    }
-  
-    add(item) {
-      this.items[item] = true;
-    }
-  
-    delete(item) {
-      if (this.has(item)) {
-        delete this.items[item];
-        return true;
-      }
-      return false;
-    }
-  
-    has(item) {
-      return this.items.hasOwnProperty(item);
-    }
-  
-    isEmpty() {
-      return Object.keys(this.items).length === 0;
-    }
-  
-    min() {
-      return Object.keys(this.items)[0];
-    }
+  constructor() {
+    this.items = {};
   }
-  
-  export default Set;
-  
+
+  add(item) {
+    this.items[item] = true;
+  }
+
+  delete(item) {
+    if (this.has(item)) {
+      delete this.items[item];
+      return true;
+    }
+    return false;
+  }
+
+  has(item) {
+    return this.items.hasOwnProperty(item);
+  }
+
+  isEmpty() {
+    return Object.keys(this.items).length === 0;
+  }
+
+  min() {
+    return Object.keys(this.items)[0];
+  }
+}
+
+export default Set;
